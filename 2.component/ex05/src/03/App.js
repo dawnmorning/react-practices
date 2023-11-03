dc6fb227c62fcc4769cf593e3cf8cc3d574f36a9
김종혁 <notify9637@naver.com>
2023-11-03 17:48:03 +0900

    feat: class component complete

--- 2.component/ex05/src/03/App.js
+++ 2.component/ex05/src/03/App.js
@@ -1,44 +1,58 @@
-import React, { Component } from 'react';
-import './assets/scss/App.scss'
-import Clock from './Clock';
+import React, { Component } from "react";
+import "./assets/scss/App.scss";
+import Clock from "./Clock";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentTime: this._getCurrentTime()
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTime: this._getCurrentTime(),
+    };
+  }
 
-    _getCurrentTime() {
-        const d = new Date();
-        return {
+  _getCurrentTime() {
+    const d = new Date();
+    const hours = d.getHours();
+    const minutes = d.getMinutes();
+    const seconds = d.getSeconds();
+    const session = hours >= 12 ? "pm" : "am";
 
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
 
+    return {
+      hours: formattedHours.toString(),
+      minutes: formattedMinutes,
+      seconds: formattedSeconds,
+      session: session,
+    };
+  }
 
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({
+        currentTime: this._getCurrentTime(),
+      });
+    }, 1000);
+  }
 
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
 
-        }
-    }
-
-    componentDidMount() {
-        this.intervalId = setInterval(function() {
-
-        }, 1);
-    }
-
-    
-    render() {
-        return (
-            <div className='clock-display'>
-                <h2>ex05 - Component LifeCycle Practice</h2>
-                <Clock
-                    hours={'02'}
-                    minutes={32}
-                    seconds={54}
-                    session={'pm'} 
-                    />
-            </div>
-        );
-    }
+  render() {
+    const { hours, minutes, seconds, session } = this.state.currentTime;
+    return (
+      <div className="clock-display">
+        <h2>ex05 - Component LifeCycle Practice</h2>
+        <Clock
+          hours={hours}
+          minutes={minutes}
+          seconds={seconds}
+          session={session}
+        />
+      </div>
+    );
+  }
 }
