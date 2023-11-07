54d1521743e3c55d47bbc728675c15f8dfa4c6c4
김종혁 <notify9637@naver.com>
2023-11-07 13:22:46 +0900

    window router

--- 3.routing/src/01/App.js
+++ 3.routing/src/01/App.js
@@ -1,8 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import Main from "./component/Main";
 import Gallery from "./component/Gallery";
 import Guestbook from "./component/Guestbook";
 
 export default function App() {
-    return (<div>Hash Route 직접 만들어 보기</div>);
+  const [route, setRoute] = useState("/");
+  useEffect(() => {
+    window.addEventListener("hashchange", handlerHashChage);
+    return () => {
+      window.removeEventListener("hashchange", handlerHashChage);
+    };
+  }, []);
+
+  const handlerHashChage = () => {
+    console.log(window.location.hash);
+    setRoute(window.location.hash);
+  };
+  return (() => {
+    switch (route) {
+      case "/":
+        return <Main />;
+      case "/guestbook":
+        return <Guestbook />;
+      case "/gallery":
+        return <Gallery />;
+      default:
+        return null;
+    }
+  })();
 }
