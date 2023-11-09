fde8bd2974c0a0c5f9cd6ba15ed53093e758c027
김종혁 <notify9637@naver.com>
2023-11-09 10:13:22 +0900

    useRoutes

--- 3.routing/src/07/App.js
+++ 3.routing/src/07/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Routes, Route } from "react-router";
+import { Routes, Route, useRoutes } from "react-router";
 
 import { SiteLayout } from "./layout";
 
@@ -16,17 +16,26 @@ import Error404 from "./component/error/Error404";
 import "./assets/scss/App.scss";
 
 export default function App() {
+  useRoutes([
+    { path: "/", element: <Main /> },
+    { path: "/gallery", element: <Gallery /> },
+    { path: "/guestbook", element: <Guestbook /> },
+    { path: "/about", element: <About /> },
+    { path: "/user/join", element: <Join /> },
+    { path: "/user/login", element: <Login /> },
+    { path: "/error", element: <Error404 /> },
+    { path: "*", element: <Error404 /> },
+  ]);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SiteLayout />}>
-          <Route index path="" element={<Main />} />
-          <Route path="gallery" element={<Gallery />} />
-          <Route path="guestbook" element={<Guestbook />} />
-          <Route path="about" element={<About />} />
-          <Route path="user/join" element={<Join />} />
-          <Route path="user/login" element={<Login />} />
-          <Route path="error" element={<Error />} />
+        <Route path="/" element={<Main />}>
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/guestbook" element={<Guestbook />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/user/join" element={<Join />} />
+          <Route path="/user/login" element={<Login />} />
+          <Route path="/error" element={<Error />} />
           <Route path="*" element={<Error404 />} />
         </Route>
       </Routes>
